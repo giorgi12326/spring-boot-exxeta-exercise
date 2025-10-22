b31bafdfe0aa1651966ac0895730e202d5f63505
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-10-22 16:27:10 +0400

    env var

--- target/generated-sources/annotations/org/example/productService/mapper/ProductMapperImpl.java
+++ target/generated-sources/annotations/org/example/productService/mapper/ProductMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-22T15:20:55+0400",
+    date = "2025-10-22T16:26:03+0400",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.15 (Eclipse Adoptium)"
 )
 @Component
