bc5f95f23adc5d153aba5051069ed85f6fc38d2e
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-10-22 12:46:41 +0400

    jar for db k8s

--- target/generated-sources/annotations/org/example/productService/mapper/ProductMapperImpl.java
+++ target/generated-sources/annotations/org/example/productService/mapper/ProductMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-21T13:33:28+0400",
+    date = "2025-10-22T12:43:58+0400",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.15 (Eclipse Adoptium)"
 )
 @Component
