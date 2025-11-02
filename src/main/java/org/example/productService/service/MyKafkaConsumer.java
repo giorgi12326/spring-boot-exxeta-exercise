9055359f9af294335a045abe3c4f92b5576cc7b3
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-02 12:22:45 +0400

    use logger isntead

--- src/main/java/org/example/productService/service/MyKafkaConsumer.java
+++ src/main/java/org/example/productService/service/MyKafkaConsumer.java
@@ -1,14 +1,17 @@
 package org.example.productService.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyKafkaConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(MyKafkaConsumer.class);
+
     @KafkaListener(topics = "test-topic", groupId = "my-group")
     public void listen(String message) {
-        System.out.println("Received message: " + message);
+        log.info("Received message: {}", message);
     }
-
 }
