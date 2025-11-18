e6144cd327587b9fe0c2b6d8e69d669f116621f9
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-18 19:00:05 +0400

    added cors fornt

--- src/main/java/org/example/productService/security/SecurityConfig.java
+++ src/main/java/org/example/productService/security/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of(
                             "http://localhost:5500",
-                            "http://127.0.0.1:5500"
+                            "http://127.0.0.1:5500",
+                            "http://5.189.131.28:81"
                     ));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
