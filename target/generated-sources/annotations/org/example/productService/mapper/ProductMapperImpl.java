2050b94217cc518409a0288200a76f5ef1c52096
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-25 09:13:25 +0400

    fixed compile error

--- target/generated-sources/annotations/org/example/productService/mapper/ProductMapperImpl.java
+++ target/generated-sources/annotations/org/example/productService/mapper/ProductMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-24T20:43:27+0400",
+    date = "2025-11-25T09:11:20+0400",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.15 (Eclipse Adoptium)"
 )
 @Component
